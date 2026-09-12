37aca9ece73d7a3004ec60f8e1d8220e152c2881
daniel <donotemailme@please.com>
2026-09-13 01:12:07 +0300

    feat: add goto_preview

---
+++ lua/plugins/goto_preview.lua
@@ -0,0 +1,13 @@
+vim.pack.add({
+  { src = "https://github.com/rmagatti/logger.nvim" },
+  { src = "https://github.com/rmagatti/goto-preview" },
+}, { confirm = false, load = true })
+
+require("goto-preview").setup({})
+
+vim.keymap.set("n", "gpd", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { noremap = true })
+vim.keymap.set("n", "gpt", "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", { noremap = true })
+vim.keymap.set("n", "gpi", "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", { noremap = true })
+vim.keymap.set("n", "gpD", "<cmd>lua require('goto-preview').goto_preview_declaration()<CR>", { noremap = true })
+vim.keymap.set("n", "gP", "<cmd>lua require('goto-preview').close_all_win()<CR>", { noremap = true })
+vim.keymap.set("n", "gpr", "<cmd>lua require('goto-preview').goto_preview_references()<CR>", { noremap = true })
